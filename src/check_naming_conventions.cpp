6cf7b1e905f2d91f70a2de2ce7c4a9b4f3af75b0
Christian Newman <cnewman@kent.edu>
2022-06-07 19:45:11 +0100

    Basic code to collect all identifier names into a list before doing any processing on whether it follows conventions. May decide to do this on the fly later, but putting it all into a list guarantees I'll have all information I need if we have any rules that require backtracking

--- src/check_naming_conventions.cpp
+++ src/check_naming_conventions.cpp
@@ -1,4 +1,8 @@
 #include <check_naming_conventions.hpp>
 int main(int argc, char** argv){
+        CheckNamingConventionsPolicy* cat = new CheckNamingConventionsPolicy();
+        srcSAXController control(argv[1]);
+        srcSAXEventDispatch::srcSAXEventDispatcher<> handler({cat}, false);
+        control.parse(&handler); //Start parsing	
 	return 0;
 }
--- src/check_naming_conventions.hpp
+++ src/check_naming_conventions.hpp
@@ -8,6 +8,24 @@
 #include <srcSAXEventDispatcher.hpp>
 #include <FunctionSignaturePolicy.hpp>
 
+struct IdentifierData{
+    IdentifierData(std::string type, std::string name, std::string context, std::string fileName, std::string programmingLanguageName, std::string lineNumber) 
+    : type{type}, name{name}, context{context}, fileName{fileName}, programmingLanguageName{programmingLanguageName}, lineNumber{lineNumber} {}
+    
+    std::string type;
+    std::string name;
+    std::string context;
+    std::string lineNumber;
+    std::string fileName;
+    std::string programmingLanguageName;
+
+    friend std::ostream& operator<<(std::ostream& outputStream, const IdentifierData& identifier){
+        outputStream<<identifier.type<<" "<<identifier.name<<" "<<identifier.context<<" "
+                    <<identifier.lineNumber<<" "<<identifier.fileName<<" "<<identifier.programmingLanguageName;
+        return outputStream;
+    }
+};
+
 class CheckNamingConventionsPolicy : public srcSAXEventDispatch::EventListener, public srcSAXEventDispatch::PolicyDispatcher, public srcSAXEventDispatch::PolicyListener 
 {
     public:
@@ -22,14 +40,44 @@ class CheckNamingConventionsPolicy : public srcSAXEventDispatch::EventListener, 
             using namespace srcSAXEventDispatch;
             if(typeid(DeclTypePolicy) == typeid(*policy)){
                 declarationData = *policy->Data<DeclData>();
+                if(!(declarationData.nameOfIdentifier.empty()||declarationData.nameOfType.empty())){
+                    if(ctx.IsOpen(ParserState::function)){
+
+                        CollectIdentifierTypeNameAndContext(declarationData.nameOfType, declarationData.nameOfIdentifier, "DECLARATION", ctx.currentLineNumber, 
+                                                            ctx.currentFilePath, ctx.currentFileLanguage);
+
+                    }else if(ctx.IsOpen(ParserState::classn) && !declarationData.nameOfContainingClass.empty() && !declarationData.nameOfType.empty() 
+                             && !declarationData.nameOfIdentifier.empty()){
+                        
+                        CollectIdentifierTypeNameAndContext(declarationData.nameOfType, declarationData.nameOfIdentifier, "ATTRIBUTE", ctx.currentLineNumber, 
+                                                            ctx.currentFilePath, ctx.currentFileLanguage);
+
+                    }
+                }
             }else if(typeid(ParamTypePolicy) == typeid(*policy)){
                 parameterData = *policy->Data<DeclData>();
+                if(!(parameterData.nameOfIdentifier.empty() || parameterData.nameOfType.empty())){
+
+                    CollectIdentifierTypeNameAndContext(parameterData.nameOfType, parameterData.nameOfIdentifier, "PARAMETER", ctx.currentLineNumber, 
+                                                        ctx.currentFilePath, ctx.currentFileLanguage);
+
+                }
             }else if(typeid(FunctionSignaturePolicy) == typeid(*policy)){
                 functionData = *policy->Data<SignatureData>();
+                if(!(functionData.name.empty() || functionData.returnType.empty())){
+                    CollectIdentifierTypeNameAndContext(functionData.returnType, functionData.name, "FUNCTION", ctx.currentLineNumber, 
+                                                        ctx.currentFilePath, ctx.currentFileLanguage);
+                }
             }
         }
         
         void NotifyWrite(const PolicyDispatcher *policy, srcSAXEventDispatch::srcSAXEventContext &ctx){}
+
+        void CollectIdentifierTypeNameAndContext(std::string identifierType, std::string identifierName, std::string codeContext,
+                                                 unsigned int lineNumber, std::string fileName, std::string programmingLanguageName){
+                std::cout<<identifierName<<std::endl;
+                allSystemIdentifiers.push_back(IdentifierData(identifierType, identifierName, codeContext, std::to_string(lineNumber), fileName, programmingLanguageName));
+        }        
         
     protected:
         void *DataInner() const override {
@@ -37,6 +85,7 @@ class CheckNamingConventionsPolicy : public srcSAXEventDispatch::EventListener, 
         }
         
     private:
+        std::vector<IdentifierData> allSystemIdentifiers;
         DeclTypePolicy declPolicy;
         DeclData declarationData;
 
@@ -62,6 +111,11 @@ class CheckNamingConventionsPolicy : public srcSAXEventDispatch::EventListener, 
 
             //Close events
             closeEventMap[ParserState::classn] = [this](srcSAXEventContext& ctx){
+                if(!ctx.currentClassName.empty()){
+
+                    CollectIdentifierTypeNameAndContext("class", ctx.currentClassName, "CLASS", ctx.currentLineNumber, 
+                                                        ctx.currentFilePath, ctx.currentFileLanguage);
+                }
             };
             closeEventMap[ParserState::functionblock] = [this](srcSAXEventContext& ctx){
                 ctx.dispatcher->RemoveListenerDispatch(&functionPolicy);
@@ -73,6 +127,7 @@ class CheckNamingConventionsPolicy : public srcSAXEventDispatch::EventListener, 
                 ctx.dispatcher->RemoveListenerDispatch(&paramPolicy);
             };
             closeEventMap[ParserState::archive] = [this](srcSAXEventContext& ctx) {
+                
             };
         }
 };
