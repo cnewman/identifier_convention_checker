54b0919f136020ceae903075effce7e36fc0c3e1
Christian Newman <cnewman.real@gmail.com>
2022-07-20 15:43:20 -0400

    Added detection code for heuristics. Added code to generate a report on each identifier.

--- scanner_source/scan_identifiers.py
+++ scanner_source/scan_identifiers.py
@@ -3,7 +3,17 @@ from enum import Enum
 from spiral import ronin
 import inflect
 
-inflect = inflect.engine()
+class FinalIdentifierReport:
+    def __init__(self):
+        self.pluralityUsage = ""
+        self.heuristicsUsage = ""
+    def setPluralityMessage():
+        pass
+    def setHeuristicsMessage():
+        pass
+    def __str__(self):
+        finalReportString = self.pluralityUsage + "\n" + self.heuristicsUsage
+        return finalReportString
 
 class NameConventionCharacteristics:
     def __init__(self):
@@ -13,8 +23,6 @@ class NameConventionCharacteristics:
         self.usesAllCapitals = False
         self.usesAllLowercase = False
         self.usesMixedStyles = False
-
-identifierNames = []
 
 class CONTEXTS(Enum):
     DECLARATION = 0
@@ -32,6 +40,8 @@ collectionTypeDict = {}
 primitiveTypeDict = {"int", "char", "long", "float", "double"}
 
 
+inflect = inflect.engine()
+
 def CheckIfIdentifierHasCollectionType(identifierData):
     #If the identifier was used with subscript, it's probably a collection
     if identifierData['array'] == '1':
@@ -43,7 +53,7 @@ def CheckIfIdentifierHasCollectionType(identifierData):
     
     return False
 
-def CheckLocalIdentifier(identifierData):
+def CheckHeuristics(identifierData, finalReport):
     currentIdentifierCharacteristics = NameConventionCharacteristics()
     
     underscoreUsages = []
@@ -57,24 +67,45 @@ def CheckLocalIdentifier(identifierData):
             capitalUsages.append(True)
         if character.islower():
             lowercaseUsages.append(True)
-
-    split_identifier_data = ronin.split(identifierData['name'])
     
+    reportString = []
+    if any(underscoreUsages):
+        reportString.append("underscores")
+    if any(capitalUsages):
+        reportString.append("upper case letters")
+    if not all(lowercaseUsages):
+        reportString.append("lower case letters")
+    
+    if len(reportString) == 3:
+        finalReport.heuristicsUsage = ("{identifierName} mixes styles, containing {heuristics}. Please follow the style guidelines."
+                                      .format(identifierName=identifierData['name'], heuristics=",".join(reportString)))
+    elif any(underscoreUsages) and any(capitalUsages):
+        finalReport.heuristicsUsage = ("{identifierName} mixes styles, containing {heuristics}. Please follow the style guidelines."
+                                      .format(identifierName=identifierData['name'], heuristics=",".join(reportString)))
+    return finalReport
+
+def CheckTypeVersusPlurality(identifierData, finalReport):
+    splitIdentifierData = ronin.split(identifierData['name'])
     # First a check to see if identifier name plurality matches its type. If it is a plural identifier,
     # But its type doesn't look like a collection, then this is a linguistic anti-pattern
-    isItPlural = inflect.singular_noun(split_identifier_data[-1])
+    isItPlural = inflect.singular_noun(splitIdentifierData[-1])
     if(isItPlural != False): #Inflect is telling us that this word is plural.
         shouldIdentifierBePlural = CheckIfIdentifierHasCollectionType(identifierData)
         if shouldIdentifierBePlural != True:
-            return "Plural identifier {identifier} has a non-collection type {typename}".format(identifier=identifierData['name'], typename=identifierData['type'])
+            finalReport.pluralityUsage = "Plural identifier {identifier} has a non-collection type {typename}".format(identifier=identifierData['name'], typename=identifierData['type'])
+            return finalReport
     else: #Inflect is telling us that this word is singular.
         shouldIdentifierBePlural = CheckIfIdentifierHasCollectionType(identifierData)
         if shouldIdentifierBePlural != False:
-            return "Singular identifier {identifier} has a collection type {typename}".format(identifier=identifierData['name'], typename=identifierData['type'])
-    
-    
-    #print("{name} in {identifier} is {plurality}".format(name=split_identifier_data[-1], identifier=split_identifier_data, plurality=inflect.singular_noun(split_identifier_data[-1])))
+            finalReport.pluralityUsage = "Singular identifier {identifier} has a collection type {typename}".format(identifier=identifierData['name'], typename=identifierData['type'])
+            return finalReport
 
+def CheckLocalIdentifier(identifierData):
+    finalReport = FinalIdentifierReport()
+    finalReport = CheckHeuristics(identifierData, finalReport)
+    finalReport = CheckTypeVersusPlurality(identifierData, finalReport)
+    return finalReport
+    #print("{name} in {identifier} is {plurality}".format(name=splitIdentifierData[-1], identifier=splitIdentifierData, plurality=inflect.singular_noun(splitIdentifierData[-1])))
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as identifier_file:
