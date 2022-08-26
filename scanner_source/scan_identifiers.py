2e21de6a0840578e724ecde2df852c9aa40bbfc3
Christian Newman <cnewman@kent.edu>
2022-08-26 15:21:23 -0400

    Adding some artifacts to help test the checker

--- scanner_source/scan_identifiers.py
+++ scanner_source/scan_identifiers.py
@@ -45,7 +45,7 @@ contextsDict = {"DECLARATION": CONTEXTS.DECLARATION,
 antiPatternDict = {
     "TERM LENGTH" : "{identifierName} has less than 3 characters in it. Typically, identifiers should be made up of dictionary terms."
                                     "Please follow the style guidelines.",
-    "DICTIONARY TERM" : "{identifierName} is not a dictioanary term.",
+    "DICTIONARY TERM" : "{identifierName} is not a dictionary term.",
     "PLURAL MISUSE" : "Plural identifier {identifier} has a non-collection type {typename}",
     "SINGULAR MISUSE" : "Singular identifier {identifier} has a collection type {typename}",
     "MIXED STYLES" : "{identifierName} mixes styles, containing {heuristics}. Please follow the style guidelines."
@@ -53,6 +53,7 @@ antiPatternDict = {
 
 primitiveTypeDict = {"int", "char", "long", "float", "double"}
 genericTerms = {"value", "result", "pointer", "output", "input", "content", "ptr", "in", "out", "val", "res", "begin", "end", "start", "finish", "tok", "token"}
+collectiontypeDict = {"vector", "list", "set", "dictionary", "map"}
 
 inflect = inflect.engine()
 englishDictionary = enchant.Dict("en_US")
@@ -75,6 +76,9 @@ def CheckIfIdentifierHasCollectionType(identifierData):
     
     #If the identifier is a pointer and has a primitive type, then it is probably a collection (in C/C++)
     if (identifierData['pointer'] == '1') and (identifierData['type'].lower() in primitiveTypeDict):
+        return True
+    
+    if identifierData['type'] in collectiontypeDict:
         return True
     
     return False
@@ -106,6 +110,7 @@ def CheckHeuristics(identifierData, finalReport):
         finalReport.heuristicsUsage = (antiPatternDict["MIXED STYLES"].format(identifierName=identifierData['name'], heuristics=",".join(reportString)))
     elif any(underscoreUsages) and any(capitalUsages):
         finalReport.heuristicsUsage = (antiPatternDict["MIXED STYLES"].format(identifierName=identifierData['name'], heuristics=",".join(reportString)))
+    
     return finalReport
 
 def CheckTypeVersusPlurality(identifierData, finalReport):
---
+++ tests/code_artifacts/code_containing_identifiers_to_test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int function_with_mixed_heuristics(){
+	//first some tests for heuristics
+	int token_count;
+	int tokenMax_count;
+	int Token;
+}
+
+int function_with_consistent_underscore_heuristics(){
+	int token_count;
+	int token_max;
+	int token;
+}
+
+int functionWithConsistentCamelCaseHeuristics(){
+	int tokenCount;
+	int tokenMax;
+	int token;
+}
+
+void functionWithSingularPluralIssues(){
+	std::vector<std::string> token;
+	int tokens;
+	std::vector<std::string> names;
+	int name;
+}
+
+void functionWithDictionaryTermIssues(){
+	int tkn;
+	int nme;
+}
