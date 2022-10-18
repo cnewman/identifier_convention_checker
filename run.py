d6dd0f6baa616c734216eceebdde43fd791e0dbf
Christian Newman <cnewman@kent.edu>
2022-10-18 15:28:29 -0400

    Enable checker to run on files or directories

--- run.py
+++ run.py
@@ -5,10 +5,12 @@ if __name__ == '__main__':
     srcml_process = subprocess.Popen(['srcml', '--position', sys.argv[1]], stdout=subprocess.PIPE)
     srcml_out = srcml_process.communicate()
     
-    extract_identifiers_process = subprocess.Popen(['build/bin/checkidentifiers', srcml_out[0].decode('utf-8').strip()], stdout=subprocess.PIPE)
+    extract_identifiers_process = subprocess.Popen(['build/bin/checkidentifiers'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    extract_identifiers_process.stdin.write(srcml_out[0])
     extract_identifiers_process_out = extract_identifiers_process.communicate()
     
     identifier_csv_reader = csv.DictReader(extract_identifiers_process_out[0].decode('utf-8').strip().splitlines())
+    
     for row in identifier_csv_reader:
         if identifier_analysis.contextsDict.get(row['context']) == identifier_analysis.CONTEXTS.DECLARATION:
             identifierAppraisal = identifier_analysis.CheckLocalIdentifier(row)
--- src/check_naming_conventions.cpp
+++ src/check_naming_conventions.cpp
@@ -1,9 +1,12 @@
 #include <check_naming_conventions.hpp>
 #include <string>
+#include <iterator>
+#include <iostream>
 int main(int argc, char** argv){
         CheckNamingConventionsPolicy* cat = new CheckNamingConventionsPolicy();
-        std::string buf = argv[1];
-        srcSAXController control(buf);
+        std::istreambuf_iterator<char> begin{std::cin}, end;
+        std::string srcML_string(begin, end);
+        srcSAXController control(srcML_string);
         srcSAXEventDispatch::srcSAXEventDispatcher<> handler({cat}, false);
         control.parse(&handler); //Start parsing	
 	return 0;
